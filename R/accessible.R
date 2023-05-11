c59001f4f8b1290be0420ba6748fbd5b8de35429
JooYoung Seo <jseo1005@illinois.edu>
2023-05-11 16:07:14 -0500

    linting

--- R/accessible.R
+++ R/accessible.R
@@ -1,14 +1,14 @@
 accessible <- function(x) {
-    UseMethod("accessible")
+  UseMethod("accessible")
 }
 
 
 accessible.ggplot <- function(x) {
-    # see `BrailleR::VI()` function
-    ## check if x == geom_bar
-    ### then export x as an svg using gridSVG
-    ### interweave this svg with JS by using tempdir, usethis, and serve the live html
-    ### Or, use htmltools::tags$object() to embed when in Rmd output.
+  # see `BrailleR::VI()` function
+  ## check if x == geom_bar
+  ### then export x as an svg using gridSVG
+  ### interweave this svg with JS by using tempdir, usethis, and serve the live html
+  ### Or, use htmltools::tags$object() to embed when in Rmd output.
 }
 
 # to do
@@ -16,14 +16,14 @@ accessible.ggplot <- function(x) {
 
 
 print.ggplot <- function(...) {
-    if (interactive()) {
-        svgpath <- htmltools::plotTag(
-            ggplot2:::print.ggplot(...), "Plot object",
-            device = svglite::svglite,
-            mimeType = "image/svg+xml",
-            attribs = list(id = "my-chart")
-        )
+  if (interactive()) {
+    svgpath <- htmltools::plotTag(
+      ggplot2:::print.ggplot(...), "Plot object",
+      device = svglite::svglite,
+      mimeType = "image/svg+xml",
+      attribs = list(id = "my-chart")
+    )
 
-        print(svgpath)
-    }
+    print(svgpath)
+  }
 }
--- R/scatterplot.R
+++ R/scatterplot.R
@@ -2,14 +2,14 @@
 NULL
 
 plot.numeric <- function(...) {
-    s <- svglite::svgstring(standalone = FALSE, id = "plot")
-    plot.default(...)
-    svg <- s()
-    dev.off()
+  s <- svglite::svgstring(standalone = FALSE, id = "plot")
+  plot.default(...)
+  svg <- s()
+  dev.off()
 
-    file <- create_wave(...)
+  file <- create_wave(...)
 
-    print(html_tag_audio(file), browse = TRUE)
+  print(html_tag_audio(file), browse = TRUE)
 }
 
 
@@ -17,24 +17,24 @@ plot.numeric <- function(...) {
 
 #' helper function saving sonified data as temp wav file
 create_wave <- function(...) {
-    suppressWarnings(
-        wav <- sonify::sonify(..., play = FALSE)
-    )
-    file <- tempfile(fileext = ".wav")
-    tuneR::writeWave(wav, filename = file)
-    invisible(file)
+  suppressWarnings(
+    wav <- sonify::sonify(..., play = FALSE)
+  )
+  file <- tempfile(fileext = ".wav")
+  tuneR::writeWave(wav, filename = file)
+  invisible(file)
 }
 
 #' helper function for creating html audio tag
 html_tag_audio <- function(file, type = c("wav")) {
-    type <- match.arg(type)
-    htmltools::tags$audio(
-        controls = "",
-        htmltools::tags$source(
-            src = file,
-            type = glue::glue("audio/{type}", type = type)
-        )
+  type <- match.arg(type)
+  htmltools::tags$audio(
+    controls = "",
+    htmltools::tags$source(
+      src = file,
+      type = glue::glue("audio/{type}", type = type)
     )
+  )
 }
 
 
@@ -49,9 +49,9 @@ abs(corr_coef)
 # sign(x)
 
 direction <- function(x) {
-    switch(sign(x),
-        1 ~ "positive",
-        0 ~ "no",
-        -1 ~ "negative"
-    )
+  switch(sign(x),
+    1 ~ "positive",
+    0 ~ "no",
+    -1 ~ "negative"
+  )
 }
